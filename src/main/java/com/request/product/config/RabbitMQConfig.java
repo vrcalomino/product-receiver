972c15ce140e5ed295e0aba867a73252b47d3934
vrcalomino <vrcalomino@gmail.com>
2024-07-20 09:35:30 -0300

    add rabbitmq config

---
+++ src/main/java/com/request/product/config/RabbitMQConfig.java
@@ -0,0 +1,48 @@
+package com.request.product.config;
+
+import com.request.product.model.RequestInformation;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.DefaultClassMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_NAME = "productRequestQueue";
+
+    @Bean
+    public Queue queue(){
+        return new Queue(QUEUE_NAME, false);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonToMapMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public MessageConverter jsonToMapMessageConverter(){
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+        DefaultClassMapper classMapper = new DefaultClassMapper();
+        classMapper.setDefaultType(RequestInformation.class);
+        converter.setClassMapper(classMapper);
+        classMapper.setTrustedPackages("*");
+        return converter;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jsonToMapMessageConverter());
+        return factory;
+    }
+}
