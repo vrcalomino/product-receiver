19b48d4ed6a40052369f5e5438064c65471d1a9b
vrcalomino <vrcalomino@gmail.com>
2024-07-20 09:38:31 -0300

    add request information

---
+++ src/main/java/com/request/product/model/RequestInformation.java
@@ -0,0 +1,15 @@
+package com.request.product.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestInformation implements Serializable {
+    private String email;
+    private Long product_id;
+}
