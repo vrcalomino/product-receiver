daae42f276a83ea92646dafe6413411aa3efe15f
vrcalomino <vrcalomino@gmail.com>
2024-07-22 14:36:27 -0300

    fix unit test for receiver

--- src/test/java/com/request/product/model/service/RabbitMQReceiverTest.java
+++ src/test/java/com/request/product/model/service/RabbitMQReceiverTest.java
@@ -1,11 +1,16 @@
 package com.request.product.model.service;
 
+import com.request.product.service.ProductAPIFetcher;
 import com.request.product.service.RabbitMQReceiver;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class RabbitMQReceiverTest {
@@ -13,8 +18,13 @@ public class RabbitMQReceiverTest {
     @InjectMocks
     private RabbitMQReceiver receiver;
 
+    @Mock
+    private ProductAPIFetcher productAPIFetcher;
+
     @Test
-    public void receiveWithoutRequest() {
-        Assertions.assertThrows(Exception.class, ()->receiver.receive(null));
+    public void receiveWithoutRequest() throws Exception {
+        receiver.receive(null);
+
+        verify(productAPIFetcher, Mockito.never()).fetchSingleProduct(Mockito.anyLong());
     }
 }
